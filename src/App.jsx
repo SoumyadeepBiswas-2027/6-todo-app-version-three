8ce947f7bc6f458135264e120def634a0be35e86
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-06-15 04:12:12 +0530

    Updated todos: removed old components, added new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
 import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
-import TodoItem1 from "./components/TodoItem1";
-import TodoItem2 from "./components/TodoItem2";
 import "./App.css";
+//imports andc components of TodoItem1 & TodoItem2 are removed as we now use both through single TodoItem component
+// import TodoItem from "./components/TodoItem";
+import TodoItems from "./components/TodoItems";
 
 function App() {
+  const todoItems = [{
+   name:'Buy Milk',
+   dueDate:'4/10/2023',
+  },
+
+  {
+   name:'Go to College',
+   dueDate:'4/10/2023',
+  },
+
+  {
+   name:'do course work',
+   dueDate:'4/10/2023',
+  },
+
+];
   return (
     <center className="todo-container">
       <AppName />
       <AddTodo />
-      <div className="items-container">
-        <TodoItem1></TodoItem1>
-        <TodoItem2></TodoItem2>
-      </div>
+      <TodoItems todoItems={todoItems}></TodoItems>
+      {/* <div className="items-container">
+        <TodoItem  todoDate="4/10/2023" todoName="Buy Milk"></TodoItem>
+         <TodoItem  todoDate="4/10/2023" todoName="Go to College"></TodoItem>
+        {/* <TodoItem1></TodoItem1>
+        <TodoItem2></TodoItem2> */}
+      {/* </div> */} 
     </center>
   );
 }
---
+++ src/components/TodoItems.jsx
@@ -0,0 +1,9 @@
+import TodoItem from "./TodoItem";
+const TodoItems = ({todoItems}) =>{
+  return <div className="items-container">
+    
+        <TodoItem  todoDate="4/10/2023" todoName="Buy Milk"></TodoItem>
+         <TodoItem  todoDate="4/10/2023" todoName="Go to College"></TodoItem>
+      </div>
+}
+export default TodoItems;
