7fc5e79cbbaace1c4d269131212b9030b49986ec
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-06-15 16:05:22 +0530

    Style updates and component changes

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,9 +1,11 @@
 import TodoItem from "./TodoItem";
+import styles from "./TodoItems.module.css";
 const TodoItems = ({todoItems}) =>{
-  return <div className="items-container">
-    
-        <TodoItem  todoDate="4/10/2023" todoName="Buy Milk"></TodoItem>
-         <TodoItem  todoDate="4/10/2023" todoName="Go to College"></TodoItem>
+  return <div className={styles.itemsContainer}>
+    {todoItems.map(item => <TodoItem  todoDate={item.dueDate} todoName={item.name}></TodoItem>)}
+        {/* <TodoItem  todoDate="4/10/2023" todoName="Buy Milk"></TodoItem>
+         <TodoItem  todoDate="4/10/2023" todoName="Go to College"></TodoItem> */}
+          {/* in previous code ,we hardcoded the functions,but then we update it and made single component ,for the functions to be viewed */}
       </div>
 }
 export default TodoItems;
