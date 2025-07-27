ca6c4c9875d9aeeaef258e98d534adf66523c42c
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-07-28 02:32:57 +0530

    useRef is learnt

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,40 +1,67 @@
-import { useState } from "react";
+import {useRef} from "react";
+// import { useState} from "react";
 import { BiSolidCommentAdd } from "react-icons/bi";
 
 function AddTodo({onNewItem}) {
 
-const [todoName,setTodoName] = useState("");
-const [dueDate,setdueDate] = useState("");
+// const [todoName,setTodoName] = useState("");
+// const [dueDate,setdueDate] = useState("");
+const todoNameElement= useRef(0);
+const dueDateElement= useRef(0);
 
-const handleNameChange = (event) =>{
-setTodoName(event.target.value);
-};
+// const handleNameChange = (event) =>{
+// setTodoName(event.target.value);
 
-const handleDateChange = (event) =>{
- setdueDate(event.target.value);
-};
+// };
 
-const handleAddButtonClicked =()=>{
+// const handleDateChange = (event) =>{
+//  setdueDate(event.target.value);
+ 
+// };
+
+const handleAddButtonClicked =(event)=>{
+  event.preventDefault();
+  const todoName = todoNameElement.current.value;
+  const dueDate = dueDateElement.current.value;
+  todoNameElement.current.value = "";
+  dueDateElement.current.value = "";
  onNewItem(todoName,dueDate);
- setdueDate("");
- setTodoName("");
-}
+//  setdueDate("");
+//  setTodoName("");
+ }
 
   return (
     <div className="container text-center">
-      <div className="row kg-row">
+      <form className="row kg-row"
+      onSubmit={handleAddButtonClicked}>
+        
         <div className="col-6">
-          <input type="text" placeholder="Enter Todo Here" value={todoName} onChange={ handleNameChange}/>
+          <input type="text" 
+          ref={todoNameElement}
+          placeholder="Enter Todo Here" 
+          //value={todoName} 
+          // onChange={ handleNameChange}
+          />
         </div>
+
         <div className="col-4">
-          <input type="date" value={dueDate} onChange={handleDateChange} />
+          <input type="date"
+          ref={ dueDateElement}
+           //value={dueDate} 
+          // onChange={handleDateChange} 
+           />
         </div>
+
         <div className="col-2">
-          <button type="button" className="btn btn-success kg-button" onClick={handleAddButtonClicked}>
+          <button  
+          // onSubmit={handleAddButtonClicked}
+          className="btn btn-success kg-button" 
+          //onClick={handleAddButtonClicked} 
+          >
             <BiSolidCommentAdd />
           </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
